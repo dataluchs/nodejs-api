c71614ac9a1c850b2efd3e4c4b379e4860066809
t1msn <tim_stehle@posteo.de>
2020-04-21 00:15:46 +0200

    add login controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -23,3 +23,36 @@ exports.register = (req, res) => {
     });
   });
 };
+
+exports.login = (req, res, next) => {
+  User.findOne({
+    username: req.body.username,
+  }).exec((err, user) => {
+    if (err) {
+      res.status(500).send({ message: err });
+      return;
+    }
+    if (!user) {
+      return res.status(404).send({ message: 'User not found.' });
+    }
+    // check valid password
+    var validPassword = bcrypt.compareSync(req.body.password, user.password);
+    if (!validPassword) {
+      return res.status(401).send({
+        accessToken: null,
+        message: 'Password not correct.',
+      });
+    }
+    // token
+    var token = jwt.sign({ id: user.id }, config.secret, {
+      expiresIn: 40000,
+    });
+
+    res.status(200).send({
+      id: user._id,
+      username: user.username,
+      email: user.email,
+      accessToken: token,
+    });
+  });
+};
